f0b14b4e3ded3d703ec52edcf8b1aa21d0c933dc
Ajay Vikhram S M <cb.en.u4ece21249@cb.students.amrita.edu>
2023-08-23 21:44:38 +0530

    Third commit

--- public/script.js
+++ public/script.js
@@ -6,16 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const sendButton = document.getElementById('sendButton');
 
   sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    if (message.trim() !== '') {
-      socket.emit('chat message', message);
-      messageInput.value = '';
-    }
+      const message = messageInput.value;
+      if (message.trim() !== '') {
+          // Create a message box element
+          const messageBox = document.createElement('div');
+          messageBox.className = 'message-box';
+          messageBox.textContent = message;
+
+          // Append the message box to the messages container
+          messages.appendChild(messageBox);
+
+          // Emit the message to the server
+          socket.emit('chat message', message);
+
+          // Clear the input field
+          messageInput.value = '';
+      }
   });
 
   socket.on('chat message', (message) => {
-    const li = document.createElement('li');
-    li.textContent = message;
-    messages.appendChild(li);
+      // Create a message box element for received messages
+      const messageBox = document.createElement('div');
+      messageBox.className = 'message-box';
+      messageBox.textContent = message;
+
+      // Append the message box to the messages container
+      messages.appendChild(messageBox);
   });
 });
