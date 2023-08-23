0210d642a42d2c0aa11f90fae0b0ad056d1509c0
Ajay Vikhram S M <cb.en.u4ece21249@cb.students.amrita.edu>
2023-08-23 21:20:31 +0530

    second commit

--- public/script.js
+++ public/script.js
@@ -1,23 +1,21 @@
-// public/script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io();
-  
-    const messages = document.getElementById('messages');
-    const messageInput = document.getElementById('messageInput');
-    const sendButton = document.getElementById('sendButton');
-  
-    sendButton.addEventListener('click', () => {
-      const message = messageInput.value;
-      if (message.trim() !== '') {
-        socket.emit('chat message', message);
-        messageInput.value = '';
-      }
-    });
-  
-    socket.on('chat message', (message) => {
-      const li = document.createElement('li');
-      li.textContent = message;
-      messages.appendChild(li);
-    });
+  const socket = io();
+
+  const messages = document.getElementById('messages');
+  const messageInput = document.getElementById('messageInput');
+  const sendButton = document.getElementById('sendButton');
+
+  sendButton.addEventListener('click', () => {
+    const message = messageInput.value;
+    if (message.trim() !== '') {
+      socket.emit('chat message', message);
+      messageInput.value = '';
+    }
   });
-  
+
+  socket.on('chat message', (message) => {
+    const li = document.createElement('li');
+    li.textContent = message;
+    messages.appendChild(li);
+  });
+});
